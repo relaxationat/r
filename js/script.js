6b3914fb8316707eb70d34cc43110920f2627c77
relaxation <104532440+relaxationat@users.noreply.github.com>
2022-08-10 15:34:05 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,23 +1,23 @@
-let menu =  document.querySelector(".menu ul");/// menu
+let menu = document.querySelector(".menu ul"); /// menu
 let btn = document.querySelector(".menu button"); /// menu button
 
-///close menu
-const closeMenu = () =>{
-	menu.style.display = "none";
-	btn.className = "menu-button"
-	btn.innerText = "="
+/// close menu
+const closeMenu = () => {
+   menu.style.display = "none";
+   btn.className = "menu-button"
+   btn.innerText = "="
 }
 
 /// responsive menu
 const mobileMenu = () => {
-	btn.className = "menu-button-clicked";
-	menu.style.display = "block";
+   btn.className = "menu-button-clicked";
+   menu.style.display = "block";
 
-	if (btn.innerText === "~") {
-		closeMenu();
-		return;
-	}
-	btn.innerText = "~"
+   if (btn.innerText === "~") {
+      closeMenu();
+      return;
+   }
+   btn.innerText = "~"
 }
 
 /// close menu when clicked link
@@ -25,42 +25,39 @@ const mobileMenu = () => {
 let linkMenu = document.querySelectorAll(".menu-right a");
 
 
-if (window.innerWidth<601) {
-	linkMenu.forEach((e)=>{
-		e.addEventListener("click",closeMenu);
-	});
+if (window.innerWidth < 601) {
+   linkMenu.forEach((e) => {
+      e.addEventListener("click", closeMenu);
+   });
 }
 
 /// animation hrAnimation
 
-const hrAnimation = (e) =>{
-	hr = e.target.nextElementSibling;
-	hr.className = "hovered";
+const hrAnimation = (e) => {
+   hr = e.target.nextElementSibling;
+   hr.className = "hovered";
 }
 
 
 /// menu animation
 
-if (window.innerWidth>600) {
-	linkMenu.forEach((e)=>{
-		e.addEventListener("mouseover",hrAnimation);
-		e.addEventListener("mouseout",(e)=>{
-			hr = e.target.nextElementSibling;
-			hr.className = "not-hover";
-		});
-	});
+if (window.innerWidth > 600) {
+   linkMenu.forEach((e) => {
+      e.addEventListener("mouseover", hrAnimation);
+      e.addEventListener("mouseout", (e) => {
+         hr = e.target.nextElementSibling;
+         hr.className = "not-hover";
+      });
+   });
 }
 
 
 /// animation fadeMove
 
 logo = document.querySelector(".after-menu .fadeMove");
-let animationInterval = setInterval(()=>{
-	logo.classList.remove("fadeMove");
-},400);
-
-
-
+let animationInterval = setInterval(() => {
+   logo.classList.remove("fadeMove");
+}, 400);
 
 
 /// animation moveAnimation
@@ -81,49 +78,48 @@ window.onscroll = () =>{
 /// popUpModal 
 let prices = document.querySelector(".price img");
 let overlay = document.querySelector(".overlay");
-const popUpModal = () =>{
-	prices.style.display = "block";
-	overlay.style.display = "block";
+const popUpModal = () => {
+   prices.style.display = "block";
+   overlay.style.display = "block";
 
-	overlay.addEventListener("click", () =>{
+   overlay.addEventListener("click", () => {
 
-	prices.style.display = "none";
-	overlay.style.display = "none";
+      prices.style.display = "none";
+      overlay.style.display = "none";
 
-	});
+   });
 
-	document.addEventListener("keydown", key =>{
-			if(key.keyCode === 27){
-				prices.style.display = "none";
-				overlay.style.display = "none";
-			}	
-	});
+   document.addEventListener("keydown", key => {
+      if (key.keyCode === 27) {
+         prices.style.display = "none";
+         overlay.style.display = "none";
+      }
+   });
 
 }
 
 /// gallery
 
 
-const popUpImage = (img, type) =>{
+const popUpImage = (img, type) => {
 
-	overlay.style.display = "block";
-	document.querySelector(`.${type}Img`).style.display = "block";
-	document.querySelector(`.${type}Img`).style.padding = "0";
-	document.querySelector(`.${type}Img`).setAttribute("src", img.getAttribute("src"));
+   overlay.style.display = "block";
+   document.querySelector(`.${type}Img`).style.display = "block";
+   document.querySelector(`.${type}Img`).style.padding = "0";
+   document.querySelector(`.${type}Img`).setAttribute("src", img.getAttribute("src"));
 
-	overlay.addEventListener("click", () =>{
-		overlay.style.display = "none";
-		document.querySelector(`.${type}Img`).style.display = "none";
-	});
+   overlay.addEventListener("click", () => {
+      overlay.style.display = "none";
+      document.querySelector(`.${type}Img`).style.display = "none";
+   });
 
-	document.addEventListener("keydown", key =>{
-			if(key.keyCode === 27){
-				document.querySelector(`.${type}Img`).style.display = "none";
-				overlay.style.display = "none";
-			}	
-	});
+   document.addEventListener("keydown", key => {
+      if (key.keyCode === 27) {
+         document.querySelector(`.${type}Img`).style.display = "none";
+         overlay.style.display = "none";
+      }
+   });
 }
-
 
 
 leftArrow = document.querySelector(".leftArrow");
@@ -132,54 +128,48 @@ let slideImages = document.querySelectorAll(".row img");
 
 
 let slideSh = [];
-slideImages.forEach((image)=>{
-	image.addEventListener("click",(img)=>{
-		popUpImage(img.target, "gallery");
-	});
-	slideSh.push(image.getAttribute("src"));
+slideImages.forEach((image) => {
+   image.addEventListener("click", (img) => {
+      popUpImage(img.target, "gallery");
+   });
+   slideSh.push(image.getAttribute("src"));
 });
 
 let i = 0;
 
 slide = document.querySelector(".slideshow img");
-	slide.addEventListener("click",(image)=>{
-		popUpImage(image.target, "slide");
-	});
-
-const moveBack = (i) =>{
-	
-	slide.setAttribute("src", `${slideSh[i]}`)
-	
-}
-
-const moveFrwd = (i) =>{
-	slide.setAttribute("src", `${slideSh[i]}`)
-	
-}
-
-
-leftArrow.addEventListener("click", ()=>{
-	i=i-1;
-	if (i>=0 & i<=slideSh.length -1) {
-		moveBack(i);
-	}else{
-		i = slideSh.length -1;
-		moveBack(i);
-	}
-	
-});
-rightArrow.addEventListener("click",()=>{
-	i=i+1;
-	if (i>=0 & i<=slideSh.length -1) {
-		moveFrwd(i);
-	}else{
-		i = 0;
-		moveFrwd(i);
-	}
+slide.addEventListener("click", (image) => {
+   popUpImage(image.target, "slide");
 });
 
+const moveBack = (i) => {
+
+   slide.setAttribute("src", `${slideSh[i]}`)
+
+}
+
+const moveFrwd = (i) => {
+   slide.setAttribute("src", `${slideSh[i]}`)
+
+}
 
 
+leftArrow.addEventListener("click", () => {
+   i = i - 1;
+   if (i >= 0 & i <= slideSh.length - 1) {
+      moveBack(i);
+   } else {
+      i = slideSh.length - 1;
+      moveBack(i);
+   }
 
-
-
+});
+rightArrow.addEventListener("click", () => {
+   i = i + 1;
+   if (i >= 0 & i <= slideSh.length - 1) {
+      moveFrwd(i);
+   } else {
+      i = 0;
+      moveFrwd(i);
+   }
+});
